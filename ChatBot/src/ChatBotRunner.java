f95293b2785fcee94bc47673a43cb1d4c916fec4
Joyin <Joyin@192.168.0.110>
2017-10-29 13:18:27 -0400

    Please dont break oml ;-;

--- ChatBot/src/ChatBotRunner.java
+++ ChatBot/src/ChatBotRunner.java
@@ -11,23 +11,52 @@ public class ChatBotRunner
 	/**
 	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
 	 */
+	static boolean chosen = false;
 	public static void main(String[] args)
 	{
-		ChatBotCChen chatbot1 = new ChatBotCChen();
-		ChatBotJWong chatbot2 = new ChatBotJWong();
-		ChatBotJWu chatbot3 = new ChatBotJWu();
-		ChatBotXChen chatbot4 = new ChatBotXChen();
-		
-		System.out.println (chatbot1.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		
-
-
-		while (!statement.equals("Bye"))
+		while (!chosen)
 		{
-			System.out.println (chatbot1.getResponse(statement));
-			statement = in.nextLine();
+			System.out.println("Welcome, Summoner, to Runeterra. Please select the champion you wish to bring forth onto Summoners Rift.\n" + "\t 1. Veigar, The Tiny Master of Evil\n" + "\t 2. Amumu, The Sad Mummy");
+			Scanner Summoner = new Scanner(System.in);
+			String response = Summoner.nextLine();
+	//Names are case sensitive
+			if (response.equals("1") || response.equals("Veigar"))
+			{
+				ChatBotJWong chatbot1 = new ChatBotJWong();
+				System.out.println (chatbot1.getGreeting());
+				String statement = Summoner.nextLine();
+				while(!statement.toLowerCase().equals("bye"))
+				{
+					System.out.println(chatbot1.getResponse(statement));
+					statement = Summoner.nextLine();
+				}
+			}
+			else if (response.equals("2") || response.equals("Amumu"))
+			{
+				ChatBotCChen chatbot2 = new ChatBotCChen();
+				System.out.println (chatbot2.getGreeting());
+				String statement = Summoner.nextLine();
+				while(!statement.toLowerCase().equals("bye"))
+				{
+					System.out.println(chatbot2.getResponse(statement));
+					statement = Summoner.nextLine();
+				}
+			}
+	/**
+	*	ChatBotJWu chatbot3 = new ChatBotJWu();
+	*	ChatBotXChen chatbot4 = new ChatBotXChen();
+	*
+	*	System.out.println (chatbot1.getGreeting());
+	*	Scanner in = new Scanner (System.in);
+	*	String statement = in.nextLine();
+	*
+	*
+	*	}
+	*	while (!statement.equals("Bye"))
+	*	{
+	*		System.out.println (chatbot1.getResponse(statement));
+	*		statement = in.nextLine();
+	*/
 		}
 	}
 
